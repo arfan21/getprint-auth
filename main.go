b9ab47b72563edc49c22477f136bf2f39fb23e20
arfan21 <arfan.jadul10@gmail.com>
2021-04-19 06:43:56 +0700

    delete test line callback

--- main.go
+++ main.go
@@ -30,60 +30,7 @@ func main() {
 	route.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Getprint service authentication")
 	})
-	// route.GET("/line-callback", func(c echo.Context) error {
-	// 	code := c.QueryParam("code")
-	// 	res, err := services.VerifyIdTokenLine(context.Background(), idToken)
-	// 	if err != nil {
-	// 		return err
-	// 	}
 
-	// 	fmt.Println(res)
-	// 	fmt.Println(code)
-
-	// 	urlLine := "https://api.line.me/oauth2/v2.1/token"
-	// 	lineClientID := os.Getenv("LINE_CLIENT_ID")
-	// 	lineSecretID := os.Getenv("LINE_SECRET_ID")
-	// 	requestBody := map[string]interface{}{
-	// 		"grant_type" : "authorization_code",
-	// 		"code" : code,
-	// 		"client_id": lineClientID,
-	// 		"client_secret" : lineSecretID,
-	// 		"redirect_uri" : "https://910f089421a9.ngrok.io/",
-	// 	}
-
-	// 	jsonByte, _ := json.Marshal(requestBody)
-	// 	reqBody := strings.NewReader(`grant_type=authorization_code&code=`+code+`&client_id=`+lineClientID+`&client_secret=`+lineSecretID+`&redirect_uri=https://620e4cee05cd.ngrok.io/`)
-
-	// 	client := new(http.Client)
-	// 	req, err := http.NewRequest("POST", urlLine, reqBody)
-	// 	if err != nil{
-	// 		fmt.Println(err)
-	// 	}
-	// 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	// 	res, err := client.Do(req)
-
-	// 	if err != nil {
-	// 		fmt.Print(err)
-	// 	}
-
-	// 	defer res.Body.Close()
-	// 	body, err := ioutil.ReadAll(res.Body)
-
-	// 	if err != nil {
-	// 		fmt.Print(err)
-	// 	}
-
-	// 	decodeJSON := make(map[string]interface{})
-
-	// 	err = json.Unmarshal(body, &decodeJSON)
-
-	// 	if err != nil {
-	// 		fmt.Print(err)
-	// 	}
-
-	// 	fmt.Println(decodeJSON)
-	// 	return nil
-	// })
 	route.Static("/well-knows", "oauth")
 	authCtrl := _authCtrl.NewAuthController(db)
 	authCtrl.Routes(route)
